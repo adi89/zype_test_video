ac5d8354d5a494b28a15977762d9428f50cf33ee
Adi Singh <adityasingh89@gmail.com>
2014-05-08 23:30:06 -0400

    simplified api call

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,12 +5,8 @@ class MainController < ApplicationController
 
 
   def index
-    if params['type'] == 'youtube'
-      binding.pry
-      @video = @zype_cli.videos.all({_type: "Video::Youtube"}).first
-    else
-      @video = @zype_cli.videos.all({_type: "Video::Zype"}).first
-    end
+
+    @video = @zype_cli.videos.first
 
     if @video
       @embed = @zype_cli.videos.embed(@video._id)
--- config/initializers/zype.rb
+++ config/initializers/zype.rb
@@ -1,6 +1,6 @@
-Zype.configure do |config|
-  config.api_key = "xwE0RIUvFKBmkaianqBN7A"
-  config.host    =  "api.zype.com"
-  config.port    =  80
-  config.use_ssl = false
-end
+  Zype.configure do |config|
+    config.api_key = "IZQ9lT6FLQ_yNFvFDzinAw"
+    config.host    =  "api.zype.com"
+    config.port    =  80
+    config.use_ssl = false
+  end
